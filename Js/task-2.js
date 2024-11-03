a66a9f05d396a6e5a558e462fbb39d3670399392
Yuliya1981-art <kornienko.bineks@gmail.com>
2024-11-03 12:53:15 +0000

    Номework 1

--- Js/task-2.js
+++
@@ -1,9 +0,0 @@
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
-  return totalPrice;
-}
-
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
