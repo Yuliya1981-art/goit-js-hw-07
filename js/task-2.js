f1fee5059b3431f9fd2ca48b4d49bd586a31f2cb
Yuliya1981-art <kornienko.bineks@gmail.com>
2024-11-04 10:16:30 +0000

    Номework 1

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,8 @@
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
-  console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
-  return totalPrice;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
-getShippingMessage("Australia", 120, 50);
-getShippingMessage("Germany", 80, 20);
-getShippingMessage("Sweden", 100, 20);
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -3,10 +3,9 @@ function getElementWidth(content, padding, border) {
   const paddingWidth = parseFloat(padding) * 2;
   const borderWidth = parseFloat(border) * 2;
   const totalWidth = contentWidth + paddingWidth + borderWidth;
-  console.log(`Total width: ${totalWidth}`);
   return totalWidth;
 }
 
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
