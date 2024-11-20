a4d9c83927f2477ec4d54f1a5327cf611773efdd
Yuliya1981-art <kornienko.bineks@gmail.com>
2024-11-20 14:40:41 +0000

    homework 03

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,16 @@
-function checkForSpam(message) {
-  const normalizedMessage = message.toLowerCase();
-  return (
-    normalizedMessage.includes("spam") || normalizedMessage.includes("sale")
-  );
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (let number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
 }
 
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
