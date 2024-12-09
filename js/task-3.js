fb391c282c11e318329fd86e6bdd300ed7d03c1f
Yuliya1981-art <kornienko.bineks@gmail.com>
2024-12-09 21:28:19 +0000

    homevork js 04

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,24 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
-  for (let number of numbers) {
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
-  }
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  return filteredNumbers;
-}
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo());
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
